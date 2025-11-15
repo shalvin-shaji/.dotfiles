ed9928404491d6a6a51cf4ddc94d2819ad2fcea3
Shalvin P Shaji <shalvinpshaji.official@gmail.com>
2025-11-16 00:14:04 +0530

    Config updates

--- wezterm/theme.lua
+++ wezterm/theme.lua
@@ -1,15 +1,10 @@
+local wezterm = require("wezterm")
+
 local theme = {}
-
 function theme.apply_to_config(config)
-	config.color_scheme = "Catppuccin Mocha"
-
-	config.window_background_image_hsb = {
-		brightness = 0.3,
-		hue = 1.0,
-		saturation = 0.8,
-	}
-	config.window_background_opacity = 0.4
 	config.kde_window_background_blur = true
+	config.font = wezterm.font("AgaveNerdFont")
+	config.font_size = 14
 end
 
 return theme
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,18 +2,11 @@ local wezterm = require("wezterm")
 local mouse = require("mouse")
 local theme = require("theme")
 local tabs = require("tabs")
-local config = {}
-
-config.font = wezterm.font("JetBrains Mono Nerd Font")
-
-config.color_scheme = "Batman"
+local config = wezterm.config_builder()
 
 mouse.apply_to_config(config)
 theme.apply_to_config(config)
 tabs.apply_to_config(config)
-
--- scroll back
---
 
 config.scrollback_lines = 3500
 
